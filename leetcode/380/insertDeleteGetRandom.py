4ac9aa7221dc124380400738742d9e007afedb5f
Kim Hayeon <54922913+kimhayeon00@users.noreply.github.com>
2024-07-12 10:24:47 +0900

    Update insertDeleteGetRandom.py

--- leetcode/380/insertDeleteGetRandom.py
+++ leetcode/380/insertDeleteGetRandom.py
@@ -3,24 +3,30 @@ class RandomizedSet:
 
     def __init__(self):
         self.tmp = []
+        self.dic = {}
 
     def insert(self, val: int) -> bool:
-        if val not in self.tmp:
+        if val not in self.dic:
+            self.dic[val] = len(self.tmp)
             self.tmp.append(val)
             return True
         else:
             return False
 
     def remove(self, val: int) -> bool:
-        if val in self.tmp: 
-            self.tmp.remove(val)
+        if val in self.dic:
+            idx = self.dic[val]
+            self.tmp[idx] = self.tmp[-1]
+            self.dic[self.tmp[-1]] = idx
+            self.tmp.pop()
+            del self.dic[val]
+            
             return True
         else:
             return False
-        
+
     def getRandom(self) -> int:
         return random.choice(self.tmp)
-        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
